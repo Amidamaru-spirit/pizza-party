686d991f36cec8dc4a88bbe8915310a119694ffc
Dmitry <mahaev99@gmail.com>
2023-04-22 16:35:33 +0300

    Начало нового проекта, пицца маркета. Добавлена разметка, стили, несколько компонентов

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import './scss/app.scss';
+
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      {/* header */}
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            {/* categories */}
+            <Categories />
+            {/* sort */}
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            {/* pizza block */}
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock title="Мексиканская" price={500} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
