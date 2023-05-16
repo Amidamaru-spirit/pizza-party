2491fa416ed700b61e15ad0792b81e40c143fe2c
Dmitry <mahaev99@gmail.com>
2023-05-16 23:44:24 +0300

    Сделал пагинацию и поиск по пиццам. PS mockAPI не умеет комбинировать запросы, поэтому работает через раз.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import './scss/app.scss';
 
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import NotFound from "./pages/NotFound";
+import Header from './components/Header';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
 
 function App() {
+	const [searchValue, setSearchValue] = React.useState('');
 
-
-
-  return (
-    <div className="wrapper">
-      {/* шапка */}
-      <Header />
-      <div className="content">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="wrapper">
+			{/* шапка */}
+			<Header searchValue={searchValue} setSearchValue={setSearchValue} />
+			<div className="content">
+				<div className="container">
+					<Routes>
+						<Route path="/" element={<Home searchValue={searchValue} />} />
+						<Route path="/cart" element={<Cart />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,62 +1,69 @@
 import React from 'react';
 
-import Categories from "../components/Categories";
-import Sort from "../components/Sort";
-import PizzaBlock from "../components/PizzaBlock";
-import Skeleton from "../components/PizzaBlock/Skeleton";
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
 
-function Home() {
+function Home({ searchValue }) {
+	const [items, setItems] = React.useState([]);
+	const [isLoading, setIsLoading] = React.useState(true);
+	const [categoryId, setCategoryId] = React.useState(0);
+	const [currentPage, setCurrentPage] = React.useState(1);
+	const [sortType, setSortType] = React.useState({
+		name: 'популярности',
+		sortType: 'rating',
+	});
 
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [categoryId, setCategoryId] = React.useState(0);
-  const [sortType, setSortType] = React.useState({
-    name: 'популярности',
-    sortType: 'rating',
-  });
+	//первый рендер пицц
+	//добавили сортировки и выбор категории в запросе
+	React.useEffect(() => {
+		setIsLoading(true);
 
-  //первый рендер пицц 
-  //добавили сортировки и выбор категории в запросе
-  React.useEffect(() => {
-    setIsLoading(true);
+		const sortBy = sortType.sortProperty;
+		const order = sortType.name.includes('по убыв.') ? 'desc' : 'asc';
+		const category = categoryId > 0 ? `category=${categoryId}` : '';
+		const search = searchValue ? `&search=${searchValue}` : '';
+		fetch(
+			`https://64514a17a3221969116010b4.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+		)
+			.then((res) => res.json())
+			.then((arr) => {
+				setItems(arr);
+				setIsLoading(false);
+			});
+		window.scrollTo(0, 0);
+	}, [categoryId, sortType, searchValue, currentPage]);
 
-    const sortBy = sortType.sortProperty.replace('-', '');
-    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
-    const category = categoryId > 0 ? `category=${categoryId}` : '';
-    fetch(
-      `https://64514a17a3221969116010b4.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,)
-    .then((res) => res.json())
-    .then((arr) => {
-      setItems(arr);
-      setIsLoading(false);
-    });
-    window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+	const pizzas = items
+		.filter((obj) => {
+			if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+				return true;
+			}
+			return false;
+		})
+		.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
 
-  return (
-    <div className="container">  
-      <div className="content__top">
-        {/* категории */}
-        <Categories 
-          value={categoryId} 
-          onChangeCategory={(index) => setCategoryId(index)} />
-          {/* сортировка */}
-          <Sort 
-            value={sortType}  
-            onChangeSort={(index) => setSortType(index)} />
-      </div>
-      <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {/* pizza block */}
-        {
-          //добавляем рендер скелетонов во время загрузки страницы с фейковым массивом из шести скелетонов
-          isLoading 
-            ? [...new Array(6)].map((_, index) => <Skeleton key={index} />) 
-            : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
-        }
-      </div>
-    </div>
-  );
+	const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+
+	return (
+		<div className="container">
+			<div className="content__top">
+				{/* категории */}
+				<Categories value={categoryId} onChangeCategory={(index) => setCategoryId(index)} />
+				{/* сортировка */}
+				<Sort value={sortType} onChangeSort={(index) => setSortType(index)} />
+			</div>
+			<h2 className="content__title">Все пиццы</h2>
+			<div className="content__items">
+				{/* pizza block */}
+				{/*добавляем рендер скелетонов во время загрузки страницы с фейковым массивом из шести скелетонов */}
+				{isLoading ? skeletons : pizzas}
+			</div>
+			<Pagination onChangePage={(number) => setCurrentPage(number)} />
+		</div>
+	);
 }
 
-export default Home
+export default Home;
