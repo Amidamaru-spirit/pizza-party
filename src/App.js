9d2fe9d3eab16e8bbe89f1b72e0752260b44784d
Dmitry <mahaev99@gmail.com>
2023-05-23 20:24:49 +0300

    добавил redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
+import { increment, decrement } from './redux/slices/filterSlice';
 
 import './scss/app.scss';
 
@@ -11,25 +13,27 @@ import NotFound from './pages/NotFound';
 export const SearchContext = React.createContext('');
 
 function App() {
-	const [searchValue, setSearchValue] = React.useState('');
+  const [searchValue, setSearchValue] = React.useState('');
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
-	return (
-		<div className="wrapper">
-			{/* шапка */}
-			<SearchContext.Provider value={{ searchValue, setSearchValue }}>
-				<Header />
-				<div className="content">
-					<div className="container">
-						<Routes>
-							<Route path="/" element={<Home />} />
-							<Route path="/cart" element={<Cart />} />
-							<Route path="*" element={<NotFound />} />
-						</Routes>
-					</div>
-				</div>
-			</SearchContext.Provider>
-		</div>
-	);
+  return (
+    <div className="wrapper">
+      {/* шапка */}
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        <Header />
+        <div className="content">
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </div>
+      </SearchContext.Provider>
+    </div>
+  );
 }
 
 export default App;
